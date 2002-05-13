6bf5b38f3f0abbbbc4c67ecd89d6668d1c01fb79
zarkon <zarkon@e6076e14-1c37-db11-a760-0003470a40ca>
2002-05-13 21:07:45 +0000

    update
    git-svn-id: svn+ssh://offspring.home.noscience.net/home/iforaker/svn/cgixx/trunk@42 e6076e14-1c37-db11-a760-0003470a40ca

--- configure.pl
+++ configure.pl
@@ -1,33 +1,56 @@
 #! /usr/bin/perl
-################################################################################
+#
 # configure.pl (bootstrap cgixx)
-# Isaac W. Foraker (isaac@tazthecat.net)
 # Use on *nix platforms.
 #
-################################################################################
+# cgixx CGI C++ Class Library
+# Copyright (C) 2002 Isaac W. Foraker (isaac@tazthecat.net)
+# All Rights Reserved
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.
+# 
+# 3. Neither the name of the Author nor the names of its contributors
+#    may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# 
+# THIS SOFTWARE AND DOCUMENTATION IS PROVIDED BY THE AUTHOR AND
+# CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+# BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+# AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
 # Most of this code is plagerized from configure.pl, written by Peter J Jones
 # for use in clo++, and uses Peter's cxxtools.
-#
-################################################################################
-#
+
+#####
 # Includes
-#
-################################################################################
 use strict;
 use Getopt::Long;
 use Cwd qw(cwd chdir);
-################################################################################
-#
+
+#####
 # Constants
-#
-################################################################################
 use constant DATE		=> 'Thu May  10 4:44:54 2002';
 use constant ID			=> '$Id$';
-################################################################################
-#
+
+#####
 # Global Variables
-#
-################################################################################
 my $cwd = cwd();
 my %clo;
 my $dirsep = "/";
@@ -37,7 +60,6 @@ my $cxxflags	= "${cwd}${dirsep}tools${dirsep}cxxflags";
 my $mkmf_flags  = "--cxxflags '$cxxflags' --quiet ";
 
 my $libname	= "cgixx";
-my $contrib_dir = "${cwd}${dirsep}src${dirsep}contrib";
 my $install_spec= "doc${dirsep}install.spec";
 
 my $includes	= "--include '${cwd}${dirsep}inc' ";
@@ -46,11 +68,9 @@ my $libraries	= "--slinkwith '${cwd}${dirsep}src,$libname' ";
 my @extra_compile = (
 	"${cwd}${dirsep}src"
 );
-################################################################################
-#
+
+#####
 # Code Start
-#
-################################################################################
 $|++;
 
 if (not defined $ENV{'CXX'}) {
@@ -113,7 +133,8 @@ print "|       make install                                          |\n";
 print "|                                                             |\n";
 print "| While you wait, why not drop a note to isaac\@tazthecat.net? |\n";
 print "+-------------------------------------------------------------+\n";
-################################################################################
+
+
 sub generate_toplevel_makefile {
 	unless (open(SPEC, ">$install_spec")) {
 		print STDERR "\n$0: can't open $install_spec: $!\n";
@@ -129,7 +150,8 @@ sub generate_toplevel_makefile {
 	system("$^X $mkmf $mkmf_flags --install $install_spec --wrapper @extra_compile");
 	print ".";
 }
-################################################################################
+
+
 sub generate_library_makefile {
 	if (not chdir("$cwd${dirsep}src")) {
 		print STDERR "\n$0: can't chdir to src: $!\n";
